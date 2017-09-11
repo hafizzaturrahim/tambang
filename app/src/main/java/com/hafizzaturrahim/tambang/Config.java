fceb8f76b98d06bf36617d8729f7e99232689af8
Hafizh <hafizzaturrahim@gmail.com>
2017-09-11 20:13:01 +0700

    v2

--- app/src/main/java/com/hafizzaturrahim/tambang/Config.java
+++ app/src/main/java/com/hafizzaturrahim/tambang/Config.java
@@ -2,5 +2,5 @@ package com.hafizzaturrahim.tambang;
 
 public class Config {
 //    public static final String base_url = "http://dolanbatu.com/side";
-    public static final String base_url = "http://dolanbatu.com";
+    public static final String base_url = "http://side.madiredo.desa.id";
 }
